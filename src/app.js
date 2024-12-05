12ed986861fc14e5f87b83f533f48b533fc7cdee
Paramesh Krishna <parameshkrishna98@gmail.com>
2024-12-05 17:44:55 +0100

    feat: add validation to schema

--- src/app.js
+++ src/app.js
@@ -9,14 +9,99 @@ const PORT = "8081";
 app.post("/signup", async (req, res) => {
   try {
     const data = req.body;
-    console.log(data);
     const user = new User(data);
-    user.save();
+    await user.save();
     return res.status(200).json({ message: "User created successfully" });
   } catch (err) {
-    console.error(err);
-    res.status(400).json({
+    return res.status(400).json({
       message: "Error creating user" + err.message,
+      code: err.code,
+    });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json({
+      data: users,
+      code: 200,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Error fetching users" + err.message,
+      code: err.code,
+    });
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (user.length <= 0) {
+      return res.status(404).json({
+        message: "User not found",
+        code: 404,
+      });
+    }
+    return res.status(200).json({
+      data: user,
+      code: 200,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "Error fetching user " + err.message,
+      code: err.code,
+    });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        code: 404,
+      });
+    }
+    return res.status(200).json({
+      message: "User deleted successfully",
+      code: 200,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "Error deleting user " + err.message,
+      code: err.code,
+    });
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  const ALLOWED_KEYS = ["age", "about", "gender", "skills"];
+  if (Object.keys(ALLOWED_KEYS).every((key) => ALLOWED_KEYS.includes(key))) {
+    return res.status(400).json({
+      message: `Cant update the field ${key}`,
+      code: 400,
+    });
+  }
+  try {
+    const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        code: 404,
+      });
+    }
+    return res.status(200).json({
+      message: "User updated successfully",
+      code: 200,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "Error updating user " + err.message,
       code: err.code,
     });
   }
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,62 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid gender value");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://eu.ui-avatars.com/api/?name=John+Doe&size=250",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
